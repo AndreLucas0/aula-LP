1fd3cfa0be93168f7419d9f1fcfb30ac67569988
André Lucas Ferreira <alucas3103@outlook.com>
2025-04-16 17:01:12 -0300

    lista 04 completa

--- Listas de Exercícios/LP_Lista_04/exercicio_09_l5/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/exercicio_09_l5/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int response;
+        String response2 = "jogarnovamente", response3;
+
+        loop: //break com label
+        for (int i = 0; i <=10; i++) {
+            for (int j = 0; j <= 10; j++) {
+                Scanner in = new Scanner(System.in);
+                System.out.println(i + " x " + j + " = ");
+                response = in.nextInt();
+
+                if (response!=i*j){
+                    System.out.println("--------------------------------------");
+                    System.out.println("Você errou! infelizmente não foi dessa vez...");
+                    System.out.println("Gostaria de jogar novamente?");
+                    System.out.println("Se sim, digite jogarnovamente");
+                    response3 = in.next();
+
+                    if (response3.equals(response2)) {
+                        System.out.println("--------------------------------------");
+                        System.out.println("Muito bem, vamos recomeçar então!");
+                        i = 0;
+                        j = -1;
+                    } else {
+                        System.out.println("Que pena! até uma próxima vez!");
+                        break loop;
+                    }
+                }
+                System.out.println("--------------------------------------");
+            }
+        }
     }
 }
--- Listas de Exercícios/LP_Lista_04/exercicio_10_l5/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/exercicio_10_l5/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double n1, term;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Escreva a quantidade de termos da sequência de Fibonacci: ");
+        n1 = in.nextInt();
+
+        for (int i = 0; i <=n1 ; i++) {
+            if (i==0){
+                System.out.print("0 ");
+            } else if (i==1) {
+                System.out.print("1 ");
+            } else if (i>1) {
+                term = (1/Math.sqrt(5)) * (Math.pow(((1+Math.sqrt(5))/2), i) - Math.pow(((1-Math.sqrt(5))/2), i));
+                System.out.printf("%.0f", term);
+                System.out.print(" ");
+            }
+        }
     }
 }
